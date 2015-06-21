4728ab0cc0c45ae849e9268387706b9cf6128ce4
Andrew Clarkson <andrew@clarkson.mn>
2015-06-21 16:24:51 -0500

    implement the makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    # This represents the cached inverse of a matrix
+    inverse <- NULL
+    
+    # Sets the current value of the matrix and 
+    # marks the inverse as unsolved
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    # Returns the current value of the matrix (x)
+    get <- function() {
+        return(x)
+    }
 
+    # Sets the inverse to a specified value
+    setinverse <- function(solution) {
+        inverse <<- solution
+    }
+
+    # Gets the current value of the inverse (NULL if unsolved)
+    getinverse <- function() {
+        return(inverse)        
+    }
+
+    # Creates an list where each item is a method enclosing this 
+    # scope, somewhat like an object.
+    object <- list(
+        set        = set, 
+        get        = get, 
+        setinverse = setinverse, 
+        getinverse = getinverse
+    )
+    return(object)
 }
 
 
